e3a0cc51ae8fb88e948bbecdf6abc8dd7187475f
Ushana Goyal <ushanakgoyal@gmail.com>
2020-07-17 03:19:05 -0700

    user can edit existing tags and save changes

--- app/src/main/java/com/example/pawsibilities/fragments/EditTagDialogFragment.java
+++ app/src/main/java/com/example/pawsibilities/fragments/EditTagDialogFragment.java
@@ -1,5 +1,6 @@
 package com.example.pawsibilities.fragments;
 
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,12 +8,24 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.pawsibilities.R;
 import com.example.pawsibilities.Tag;
+import com.example.pawsibilities.databinding.FragmentEditTagBinding;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,18 +34,23 @@ import com.example.pawsibilities.Tag;
  */
 public class EditTagDialogFragment extends DialogFragment {
 
+    private static final String TAG = "EditTagDialogFragment";
     public static final String KEY_TAG = "Tag";
+    public static final String KEY_Location = "Location";
+    private FragmentEditTagBinding binding;
     private Tag tag;
+    private ParseGeoPoint userLocation;
 
     public EditTagDialogFragment() {
         // Required empty public constructor
     }
 
 
-    public static EditTagDialogFragment newInstance(Tag tag) {
+    public static EditTagDialogFragment newInstance(Tag tag, Location currentLocation) {
         EditTagDialogFragment fragment = new EditTagDialogFragment();
         Bundle args = new Bundle();
         args.putParcelable(KEY_TAG, tag);
+        args.putParcelable(KEY_Location, currentLocation);
         fragment.setArguments(args);
         return fragment;
     }
@@ -41,7 +59,8 @@ public class EditTagDialogFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_tag, container, false);
+        binding = FragmentEditTagBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
@@ -49,8 +68,65 @@ public class EditTagDialogFragment extends DialogFragment {
         super.onViewCreated(view, savedInstanceState);
         if (getArguments() != null) {
             this.tag = getArguments().getParcelable(KEY_TAG);
+            Location l = (Location) getArguments().get(KEY_Location);
+            this.userLocation = new ParseGeoPoint(l.getLatitude(), l.getLongitude());
         }
 
-        // TODO fill in overlay's functionality
+        ParseFile profile = tag.getPhoto();
+        if (profile != null) {
+            Glide.with(getContext()).load(profile.getUrl()).transform(new RoundedCorners(150)).into(binding.ivPhoto);
+        }
+
+        binding.tvName.setText(tag.getName());
+        binding.tvTimeAgo.setText("Updated " + tag.getRelativeTimeAgo());
+        binding.tvDistance.setText(tag.distanceFrom(userLocation) + " miles away");
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.directions_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spDirection.setAdapter(adapter);
+        binding.spDirection.setSelection(adapter.getPosition(tag.getDirection()));
+
+        binding.tvActive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tag.setActive(false);
+                tag.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null) {
+                            Toast.makeText(getContext(), "Unable to update", Toast.LENGTH_SHORT).show();
+                            Log.e(TAG, "Unable to mark tag outdated", e);
+                        } else {
+                            Toast.makeText(getContext(), "Updated!", Toast.LENGTH_SHORT).show();
+                            dismiss();
+                        }
+                    }
+                });
+            }
+        });
+
+        binding.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tag.setName(binding.tvName.getText().toString());
+                tag.setDirection(binding.spDirection.getSelectedItem().toString());
+
+                //
+
+                tag.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null) {
+                            Toast.makeText(getContext(), "Unable to update", Toast.LENGTH_SHORT).show();
+                            Log.e(TAG, "Unable to mark tag outdated", e);
+                        } else {
+                            Toast.makeText(getContext(), "Updated!", Toast.LENGTH_SHORT).show();
+                            dismiss();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
