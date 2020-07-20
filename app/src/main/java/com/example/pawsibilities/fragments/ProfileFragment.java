68d0902de25ee8a368b3766c222a9fe5143e1000
Ushana Goyal <ushanakgoyal@gmail.com>
2020-07-20 12:50:44 -0700

    users can select profile photo from gallery

--- app/src/main/java/com/example/pawsibilities/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/pawsibilities/fragments/ProfileFragment.java
@@ -1,15 +1,22 @@
 package com.example.pawsibilities.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
@@ -19,12 +26,17 @@ import com.example.pawsibilities.databinding.FragmentProfileBinding;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
 
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
+
 
 public class ProfileFragment extends Fragment {
 
     private static final String KEY_PROFILE = "profile";
     private ParseUser currentUser;
     private FragmentProfileBinding profileBinding;
+    private final static int PICK_PHOTO_CODE = 1046;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -48,8 +60,15 @@ public class ProfileFragment extends Fragment {
         profileBinding.tvUsername.setText(currentUser.getUsername());
         ParseFile profile = currentUser.getParseFile(KEY_PROFILE);
         if (profile != null) {
-            Glide.with(getContext()).load(profile.getUrl()).transform(new RoundedCorners(150)).into(profileBinding.ivProfile);
+            Glide.with(getContext()).load(profile.getUrl()).circleCrop().into(profileBinding.ivProfile);
         }
+
+        profileBinding.ivProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onPickPhoto(view);
+            }
+        });
 
         // log out button
         profileBinding.btnLogOut.setOnClickListener(new View.OnClickListener() {
@@ -64,4 +83,28 @@ public class ProfileFragment extends Fragment {
             }
         });
     }
+
+    // Trigger gallery selection for a photo
+    public void onPickPhoto(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((data != null) && requestCode == PICK_PHOTO_CODE) {
+            Uri photoUri = data.getData();
+            Glide.with(getContext()).load(photoUri).circleCrop().into(profileBinding.ivProfile);
+        }
+    }
 }
+
+
+
+
