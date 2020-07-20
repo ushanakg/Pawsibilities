35a15dc87ae11bfdb4784b814e4389e91f8f014e
Ushana Goyal <ushanakgoyal@gmail.com>
2020-07-20 13:19:26 -0700

    user can select and save profile photo from image gallery

--- app/src/main/java/com/example/pawsibilities/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/pawsibilities/fragments/ProfileFragment.java
@@ -13,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,9 +24,14 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.pawsibilities.LoginActivity;
 import com.example.pawsibilities.R;
 import com.example.pawsibilities.databinding.FragmentProfileBinding;
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 
 import static android.app.Activity.RESULT_OK;
@@ -34,6 +40,7 @@ import static android.app.Activity.RESULT_OK;
 public class ProfileFragment extends Fragment {
 
     private static final String KEY_PROFILE = "profile";
+    private static final String TAG = "ProfileFragment";
     private ParseUser currentUser;
     private FragmentProfileBinding profileBinding;
     private final static int PICK_PHOTO_CODE = 1046;
@@ -85,7 +92,7 @@ public class ProfileFragment extends Fragment {
     }
 
     // Trigger gallery selection for a photo
-    public void onPickPhoto(View view) {
+    private void onPickPhoto(View view) {
         Intent intent = new Intent(Intent.ACTION_PICK,
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
@@ -100,8 +107,57 @@ public class ProfileFragment extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
         if ((data != null) && requestCode == PICK_PHOTO_CODE) {
             Uri photoUri = data.getData();
+
+            // set photo in image view
             Glide.with(getContext()).load(photoUri).circleCrop().into(profileBinding.ivProfile);
+
+            Bitmap bmp = loadFromUri(photoUri);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.PNG, 50, stream);
+            byte[] byteArray = stream.toByteArray();
+
+            // save photo to user in Parse database
+            final ParseFile pf = new ParseFile("profile_pic.jpg", byteArray);
+            pf.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        saveUserProfile(pf);
+                    }
+                }
+            });
         }
+    }
+
+    private void saveUserProfile(ParseFile parseFile) {
+        currentUser.put(KEY_PROFILE, parseFile);
+        currentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "saving user profile photo failed", e);
+                } else {
+                    Toast.makeText(getContext(), "Updated!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private Bitmap loadFromUri(Uri photoUri) {
+        Bitmap image = null;
+        try {
+            if(Build.VERSION.SDK_INT > 27){
+                // on newer versions of Android, use the new decodeBitmap method
+                ImageDecoder.Source source = ImageDecoder.createSource(getContext().getContentResolver(), photoUri);
+                image = ImageDecoder.decodeBitmap(source);
+            } else {
+                // support older versions of Android by using getBitmap
+                image = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), photoUri);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
     }
 }
 
