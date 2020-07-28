d900d80f168c1f2e56b71829a91c6cc507a548af
Ushana Goyal <ushanakgoyal@gmail.com>
2020-07-28 13:14:32 -0700

    fab animation also shows when app is launched

--- app/src/main/java/com/example/pawsibilities/MainActivity.java
+++ app/src/main/java/com/example/pawsibilities/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -56,34 +58,74 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mainBinding.vpPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
+        // fab animation called when page transitions detected
+        final ViewPager.OnPageChangeListener lstnr = new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
                 if (position == PagerAdapter.MAP) {
-                    mainBinding.fabCreateTag.show();
+                    fabEnterReveal();
                 } else {
-                    mainBinding.fabCreateTag.hide();
+                    fabExitReveal();
                 }
             }
-
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
 
             @Override
             public void onPageScrollStateChanged(int state) {}
-        });
+        };
+        mainBinding.vpPager.addOnPageChangeListener(lstnr);
 
+        // fab click listener
         mainBinding.fabCreateTag.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 fabButtonClickListener.onFabClicked();
             }
         });
+
+        // set first page to map and ensure associated animations occur
+        mainBinding.vpPager.setCurrentItem(PagerAdapter.MAP);
+        mainBinding.vpPager.post(new Runnable() { // use runnable to make sure page is instantiated first
+            @Override
+            public void run() { lstnr.onPageSelected(PagerAdapter.MAP); }
+        });
     }
 
     public void setListener(FabButtonClickListener listener){
         fabButtonClickListener = listener;
+    }
+
+    private void fabEnterReveal() {
+        // get the center for the clipping circle
+        int cx = mainBinding.fabCreateTag.getMeasuredWidth() / 2;
+        int cy = mainBinding.fabCreateTag.getMeasuredHeight() / 2;
+
+        int finalRadius = mainBinding.fabCreateTag.getWidth() / 2;
+        Animator anim = ViewAnimationUtils.createCircularReveal(mainBinding.fabCreateTag, cx, cy, 0, finalRadius);
+
+        // make the view visible and start the animation
+        mainBinding.fabCreateTag.setVisibility(View.VISIBLE);
+        anim.start();
+    }
+
+    private void fabExitReveal() {
+        // get the center for the clipping circle
+        int cx = mainBinding.fabCreateTag.getMeasuredWidth() / 2;
+        int cy = mainBinding.fabCreateTag.getMeasuredHeight() / 2;
+
+        int initialRadius = mainBinding.fabCreateTag.getWidth() / 2;
+        Animator anim = ViewAnimationUtils.createCircularReveal(mainBinding.fabCreateTag, cx, cy, initialRadius, 0);
+
+        // make the view invisible when the animation is done
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                mainBinding.fabCreateTag.setVisibility(View.INVISIBLE);
+            }
+        });
+        anim.start();
     }
 
     // listener interface for floating action button click
