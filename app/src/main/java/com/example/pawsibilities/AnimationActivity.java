00f04884aa6b1459a24685f0ca15f151daa40704
Ushana Goyal <ushanakgoyal@gmail.com>
2020-08-07 14:28:52 -0700

    don't show animation for older devices (not compatible)

--- app/src/main/java/com/example/pawsibilities/AnimationActivity.java
+++ app/src/main/java/com/example/pawsibilities/AnimationActivity.java
@@ -20,39 +20,46 @@ public class AnimationActivity extends AppCompatActivity {
         binding = ActivityAnimationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setElevation(0);
+        if (android.os.Build.VERSION.SDK_INT < 21) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setElevation(0);
 
-        if (ParseUser.getCurrentUser() != null) {
-            Intent i = new Intent(AnimationActivity.this, MainActivity.class);
-            startActivity(i);
-            finish();
-        }
-
-        binding.animationView.setImageAssetsFolder("res/lottie/");
-        binding.animationView.loop(false);
-
-        binding.animationView.addAnimatorListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) { }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                // open login activity
-                Intent i = new Intent(AnimationActivity.this, LoginActivity.class);
-                Bundle bundle = ActivityOptionsCompat.makeCustomAnimation(AnimationActivity.this,
-                        android.R.anim.fade_in, android.R.anim.fade_out).toBundle();
-                startActivity(i, bundle);
+            if (ParseUser.getCurrentUser() != null) {
+                Intent i = new Intent(AnimationActivity.this, MainActivity.class);
+                startActivity(i);
                 finish();
             }
 
-            @Override
-            public void onAnimationCancel(Animator animator) { }
+            binding.animationView.setImageAssetsFolder("res/lottie/");
+            binding.animationView.loop(false);
 
-            @Override
-            public void onAnimationRepeat(Animator animator) { }
-        });
+            binding.animationView.addAnimatorListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) { }
 
-        binding.animationView.playAnimation();
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    goLoginActivity();
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) { }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) { }
+            });
+
+            binding.animationView.playAnimation();
+        } else {
+            goLoginActivity();
+        }
+    }
+
+    private void goLoginActivity() {
+        Intent i = new Intent(AnimationActivity.this, LoginActivity.class);
+        Bundle bundle = ActivityOptionsCompat.makeCustomAnimation(AnimationActivity.this,
+                android.R.anim.fade_in, android.R.anim.fade_out).toBundle();
+        startActivity(i, bundle);
+        finish();
     }
 }
